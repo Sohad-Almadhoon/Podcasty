16657c4e02980767177d0b4943fdd429c515f0e1
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2025-02-22 16:03:30 +0200

    ...

--- app/(pages)/profile/[id]/page.tsx
+++ app/(pages)/profile/[id]/page.tsx
@@ -17,7 +17,7 @@ const Profile = async (props: { params: paramsType }) => {
   if (!podcasts) return <LoaderSpinner />;
   const userInfo = await getUser();
   return (
-    <div className="min-h-screen p-8 text-white">
+    <div className="min-h-screen lg:p-8 p-5 text-white">
       <h1 className="text-2xl font-bold">Welcome, {user.username}</h1>
 
       <div className="flex items-center gap-4 my-4 mb-12">
@@ -36,9 +36,9 @@ const Profile = async (props: { params: paramsType }) => {
       <h2 className="text-xl font-semibold my-5">Your Podcasts</h2>
 
       {podcasts?.length > 0 ? (
-        <ul className="grid lg:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-5">
+        <ul className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
           {podcasts.map((podcast) => (
-            <div key={podcast.id} className="p-4 rounded-lg relative">
+            <div key={podcast.id} className="rounded-lg relative">
               <Link href={`/podcasts/${podcast.id}`}>
                 <PodcastCard podcast={podcast} />
               </Link>
